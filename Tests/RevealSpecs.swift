fbeca36ded30d0caf0e9efd06afd032ea0f61bd4
kAzec <kazecx@gmail.com>
2016-07-24 18:47:40 +0800

    Add testing dependencies.

--- Tests/RevealSpecs.swift
+++ Tests/RevealSpecs.swift
@@ -1,10 +1,10 @@
 @testable import Reveal
 
-//import Quick
-//import Nimble
-//
-//class RevealSpecs: QuickSpec {
-//    override func spec() {
-//        let node = Node<Int>()
-//    }
-//}
+import Quick
+import Nimble
+
+class RevealSpecs: QuickSpec {
+    override func spec() {
+        let node = Node<Int>()
+    }
+}
